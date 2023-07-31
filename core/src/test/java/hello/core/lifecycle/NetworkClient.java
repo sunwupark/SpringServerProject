8a0c7cb7dd2affb447239d591fa51eeae9369c2c
sunwupark <sunwu5678@gmail.com>
2023-07-31 21:58:24 +0900

    added java classes

--- core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,6 +1,9 @@
 package hello.core.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient{
 
     private String url;
     public NetworkClient(){
@@ -13,5 +16,27 @@ public class NetworkClient {
 
     public void connect(){
         System.out.println("connect  = " + url);
+    }
+
+    public void call(String message){
+        System.out.println("call  = " + url + " message = "  + message);
+    }
+
+    public void disconnect(){
+        System.out.println("close:  = " + url);
+    }
+
+    //의존 관계 주입이 끝나고
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("NetworkClient.destroy");
+        disconnect();
     }
 }
