cfb534441653ab0c83fa602ea6837c2a2d24f7af
sunwupark <sunwu5678@gmail.com>
2023-08-08 10:41:43 +0900

    dfd

--- study/ex1-hello-jpa/jpa-basic/src/main/java/hellojpa/JpaMain.java
+++ study/ex1-hello-jpa/jpa-basic/src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -14,10 +15,20 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member = new Member();
-            member.setId(2L);
-            member.setName("HelloB");
-            em.persist(member);
+            Member findMember = em.find(Member.class, 1L);
+            System.out.println("findMember.getId() = " + findMember.getId());
+            System.out.println("findMember.getName() = " + findMember.getName());
+            findMember.setName("SUNWU");
+
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                            .setFirstResult(5)
+                                    .setMaxResults(10)
+                                            .getResultList();
+
+            for (Member member : result){
+                System.out.println("member.getName() = " + member.getName());
+            }
+
             tx.commit();
         } catch (Exception e){
             tx.rollback();
