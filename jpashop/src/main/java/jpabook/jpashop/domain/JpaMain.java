aef469e18eb0c05327e62b615957072d0f3a5318
sunwupark <sunwu5678@gmail.com>
2023-08-11 10:36:27 +0900

    hi

--- jpashop/src/main/java/jpabook/jpashop/domain/JpaMain.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class JpaMain {
@@ -15,19 +16,16 @@ public class JpaMain {
         tx.begin();
 
         try {
+            Member member = new Member();
+            member.setCreatedBy("kim");
+            member.setName("user1");
+            member.setCreatedDate(LocalDateTime.now());
 
-            Movie movie = new Movie();
-            movie.setDirector("aaa");
-            movie.setActor("bbbb");
-            movie.setName("ㅂㅏ람과 함꼐 사라지다");
-
-            em.persist(movie);
+            em.persist(member);
 
             em.flush();
             em.clear();
 
-            Movie findMove = em.find(Movie.class, movie.getId());
-            System.out.println("findMove = " + findMove);
 
             tx.commit();
         } catch (Exception e){
