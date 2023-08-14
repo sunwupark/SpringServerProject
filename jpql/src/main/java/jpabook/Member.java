def3841d5476f150e0b02a1fd3ce7a8ac61c17f6
sunwupark <sunwu5678@gmail.com>
2023-08-14 10:16:16 +0900

    ghjk

---
+++ jpql/src/main/java/jpabook/Member.java
@@ -0,0 +1,40 @@
+package jpabook;
+
+import javax.persistence.*;
+
+@Entity
+public class Member {
+    @Id
+    @GeneratedValue
+    private String Id;
+    private String username;
+    private int age;
+
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String id) {
+        Id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
---
+++ jpql/src/main/java/jpabook/Team.java
@@ -0,0 +1,35 @@
+package jpabook;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Team {
+
+    @Id @GeneratedValue
+    private String id;
+    private String name;
+
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<>();
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
