676376a2549d1d0dec0cd08d95e87043c30e4d55
sunwupark <sunwu5678@gmail.com>
2023-08-17 12:19:10 +0900

    delete

--- NMW/NMW/src/main/java/NoMetroWaiting/NMW/TestRepository.java
+++
@@ -1,8 +0,0 @@
-package NoMetroWaiting.NMW;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TestRepository extends MongoRepository<Book,String> {
-}
