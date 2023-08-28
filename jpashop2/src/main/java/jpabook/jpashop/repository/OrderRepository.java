3374d93da533ec6457e80e8236d9296afa11dc89
sunwupark <sunwu5678@gmail.com>
2023-08-28 21:53:40 +0900

    dfd

--- jpashop2/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ jpashop2/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -18,15 +18,4 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-    public List<Order> findAll(OrderSearch orderSearch){
-        em.createQuery("select o from Order o join o.member m" +
-                " where o.status = :status " +
-                " and m.name like :name", Order.class)
-                .setParameter("status", orderSearch.getOrderStatus())
-                .setParameter("name", orderSearch.getMemberName())
-                .setMaxResults(1000)
-                .getResultList();
-
-    }
-
 }
