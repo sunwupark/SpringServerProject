4721a23b7555307cf34f0fd6764cd3e43c282583
SunwuPark <sunwu5678@gmail.com>
2023-09-05 15:54:21 +0900

    second in notebookdfd

--- jpashop2/src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ jpashop2/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -1,4 +1,21 @@
 package jpabook.jpashop.api;
 
-public class OrderApiController {
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class Ord
+        erApiController {
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/oroders")
+    public List<Order> ordersV1(){
+        List<Order> all = orderRepository.f
+    }
 }
