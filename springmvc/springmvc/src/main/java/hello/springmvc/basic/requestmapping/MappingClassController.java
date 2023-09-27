07973fa2bbaa63b5c3bc6c3e80e5807ead3ec293
SunwuPark <sunwu5678@gmail.com>
2023-09-27 15:38:54 +0900

    spring mvc1 start

--- springmvc/springmvc/src/main/java/hello/springmvc/basic/requestmapping/MappingClassController.java
+++ springmvc/springmvc/src/main/java/hello/springmvc/basic/requestmapping/MappingClassController.java
@@ -1,4 +1,32 @@
 package hello.springmvc.basic.requestmapping;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/mapping/users")
 public class MappingClassController {
+
+    @GetMapping
+    public String user(){
+        return "get users";
+    }
+    @PostMapping
+    public String addUser(){
+        return "posot users";
+    }
+
+    @GetMapping("/{userId}")
+    public String findUser(@PathVariable String userId){
+        return "get Users = " + userId;
+    }
+
+    @PatchMapping("/{userId}")
+    public String updateUser(@PathVariable String userId){
+        return "update Users = " + userId;
+    }
+
+    @DeleteMapping("/{userId}")
+    public String deleteUser(@PathVariable String userId){
+        return "delete Users = " + userId;
+    }
 }
